1de3ba6585894c6796b3c7faf01c12cc6a349f21
Bharath <nairbharathofficial@gmail.com>
2026-06-09 20:24:21 +0530

    fix: oAuth ID added

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: '/api',
   // Replace with your actual Google OAuth 2.0 Client ID from Google Cloud Console
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+  // googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+  googleClientId: '683123862707-ck3dlr34n9d2hn8vgb01b91btv4j8773.apps.googleusercontent.com',
+
 };
